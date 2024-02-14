4631322eef319694230a6568f5fc9901d913ac33
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-14 12:20:12 -0600

    Creates goal state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
 import Header from "./components/Header.tsx";
 import CourseGoal from "./components/CourseGoal.tsx";
-import goals from "./assets/goals.jpg";
+import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function addGoalHandler() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth!",
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
-      <Header image={{ src: goals, alt: "A list of goals" }}>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title="Learn React & TS">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={addGoalHandler}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
