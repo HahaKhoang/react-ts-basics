d097bb437872d64c349d06079560f8a1e8655ab9
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-14 14:35:35 -0600

    Connects NewGoal's form to App state

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,32 @@
-import { type FormEvent } from "react";
+import { useRef, type FormEvent } from "react";
 
-export default function NewGoal() {
-  function submitHandler(e: FormEvent) {
+type NewGoalProps = {
+  onAddGoal: (goal: string, summary: string) => void;
+};
+
+export default function NewGoal({ onAddGoal }: NewGoalProps) {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
+  function submitHandler(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+
+    e.currentTarget.reset();
+    onAddGoal(enteredGoal, enteredSummary);
   }
 
   return (
     <form onSubmit={submitHandler}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" ref={goal} />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input id="summary" type="text" />
+        <input id="summary" type="text" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
